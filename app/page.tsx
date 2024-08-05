53b37fdf57ef2cfd65c17010ee2cc63233112547
Berl treasure Campomanes <berltreasurecampomanes@Berls-MacBook-Pro.local>
2024-08-05 13:39:22 +0800

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,164 @@
+"use client";
+import { createClient } from "@supabase/supabase-js";
 import Image from "next/image";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+const serviceAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(supabaseUrl, serviceAnonKey);
+
+interface FormTypes {
+  player_a: string;
+  player_b: string;
+  contact_number: string;
+  address: string;
+}
 
 export default function Home() {
+  const [saving, setSaving] = useState(false);
+  const [registered, setRegistered] = useState(false);
+
+  const {
+    register,
+    formState: { errors },
+    reset,
+    handleSubmit,
+  } = useForm<FormTypes>({
+    mode: "onSubmit",
+  });
+
+  const onSubmit = async (formdata: FormTypes) => {
+    if (saving) return;
+
+    setSaving(true);
+
+    void handleCreate(formdata);
+  };
+
+  const handleCreate = async (formdata: FormTypes) => {
+    const params = {
+      player_a: formdata.player_a,
+      player_b: formdata.player_b,
+      contact_number: formdata.contact_number,
+      address: formdata.address,
+    };
+
+    try {
+      const { data, error } = await supabase
+        .from("pickle")
+        .insert(params)
+        .select();
+
+      if (error) throw new Error(error.message);
+
+      setSaving(false);
+      setRegistered(true);
+    } catch (error) {
+      console.error("error", error);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col items-center justify-start min-h-screen">
+      <Image alt="Pickle" src="/bg2.png" width={500} height={24} />
+      <div className="w-full flex flex-col items-center justify-start p-4">
+        <div className="bg-white text-gray-900 w-full sm:w-[500px] p-4">
+          {registered && (
+            <div className="font-mono text-center text-lg">
+              You are successfully registered.
+            </div>
+          )}
+          {!registered && (
+            <>
+              <div className="font-mono text-center text-lg">
+                Fill up the form
+              </div>
+              <div className="font-mono">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="flex flex-col space-y-4">
+                    <div>
+                      <div className="text-gray-600 font-medium text-sm">
+                        Player A
+                      </div>
+                      <div>
+                        <input
+                          {...register("player_a", { required: true })}
+                          type="text"
+                          className="w-full text-sm py-1 px-2 text-gray-600 border border-gray-300 rounded-sm focus:ring-0 focus:outline-none"
+                        />
+                        {errors.player_a && (
+                          <div className="app__error_message">
+                            Player A is required
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                    <div>
+                      <div className="text-gray-600 font-medium text-sm">
+                        Player B
+                      </div>
+                      <div>
+                        <input
+                          {...register("player_b", { required: true })}
+                          type="text"
+                          className="w-full text-sm py-1 px-2 text-gray-600 border border-gray-300 rounded-sm focus:ring-0 focus:outline-none"
+                        />
+                        {errors.player_b && (
+                          <div className="app__error_message">
+                            Player B is required
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                    <div>
+                      <div className="text-gray-600 font-medium text-sm">
+                        Contact #
+                      </div>
+                      <div>
+                        <input
+                          {...register("contact_number", { required: true })}
+                          type="text"
+                          className="w-full text-sm py-1 px-2 text-gray-600 border border-gray-300 rounded-sm focus:ring-0 focus:outline-none"
+                        />
+                        {errors.contact_number && (
+                          <div className="app__error_message">
+                            Mobile No is required
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                    <div>
+                      <div className="text-gray-600 font-medium text-sm">
+                        Address (City/Municipality)
+                      </div>
+                      <div>
+                        <input
+                          {...register("address", { required: true })}
+                          type="text"
+                          className="w-full text-sm py-1 px-2 text-gray-600 border border-gray-300 rounded-sm focus:ring-0 focus:outline-none"
+                        />
+                        {errors.address && (
+                          <div className="app__error_message">
+                            Address is required
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                    <button
+                      type="submit"
+                      className="bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-500 border border-emerald-600 font-bold px-2 py-1 text-lg text-white rounded-sm"
+                    >
+                      Register
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </>
+          )}
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
