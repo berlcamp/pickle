3fea076b3a1671dc954930b6b396dc3803faf8de
Berl treasure Campomanes <berltreasurecampomanes@Berls-MacBook-Pro.local>
2024-08-09 19:06:49 +0800

    ..

---
+++ app/classa/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-start min-h-screen">
+      <iframe
+        src="https://challonge.com/494re7me/module"
+        width="100%"
+        height="1000"
+      ></iframe>
+    </main>
+  );
+}
