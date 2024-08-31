2bd216ec5a9b3cd8814cb64092dd040a51ddec8a
Berl treasure Campomanes <berltreasurecampomanes@Berls-MacBook-Pro.local>
2024-08-31 11:12:28 +0800

    ..

--- app/classa/page.tsx
+++ app/classa/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-start min-h-screen">
       <iframe
-        src="https://challonge.com/494re7me/module"
+        src="https://challonge.com/x7n3icz3/module"
         width="100%"
         height="1000"
       ></iframe>
--- app/classc/page.tsx
+++ app/classc/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-start min-h-screen">
       <iframe
-        src="https://challonge.com/mmec2qmi/module"
+        src="https://challonge.com/oxro3mmr/module"
         width="100%"
         height="1000"
       ></iframe>
