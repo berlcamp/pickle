afd8d7359abaca99a016217143d96cfc3a85ced6
Berl treasure Campomanes <berltreasurecampomanes@Berls-MacBook-Pro.local>
2024-08-31 14:09:05 +0800

    ..

--- app/classc/page.tsx
+++ app/classc/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-start min-h-screen">
       <iframe
-        src="https://challonge.com/oxro3mmr/module"
+        src="https://challonge.com/pqrg81be/module"
         width="100%"
         height="1000"
       ></iframe>
