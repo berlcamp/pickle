c85bd7c9dfc427b2ec77a788cc6cfa3a04494300
Berl treasure Campomanes <berlcamp@gmail.com>
2025-11-11 15:54:50 +0800

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -203,6 +203,7 @@ export default function Home() {
       categoryName === 'novicemen' || categoryName === 'novicemen' ? 20 : 20
     limit = categoryName === 'imx' ? 12 : limit
     if (
+      categoryName === 'im' ||
       categoryName === 'bw' ||
       categoryName === 'novicemen' ||
       categoryName === 'novice'
